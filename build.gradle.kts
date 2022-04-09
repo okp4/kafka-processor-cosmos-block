c15f61332db151bce0ff1360fb1373d6a351c7bb
Christophe Camel <camel.christophe@gmail.com>
2022-04-09 09:20:15 +0200

    chore(deps): add kotest-framework-datatest dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest:kotest-framework-datatest:$kotestVersion")
 }
 
 tasks {
