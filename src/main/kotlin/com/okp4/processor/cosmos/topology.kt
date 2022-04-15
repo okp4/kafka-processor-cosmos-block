7fc8e2ffc7aceabd9a03852394a34f20a309904b
Christophe Camel <camel.christophe@gmail.com>
2022-04-15 10:22:19 +0200

    refactor: improve topology

--- src/main/kotlin/com/okp4/processor/cosmos/topology.kt
+++ src/main/kotlin/com/okp4/processor/cosmos/topology.kt
@@ -1,10 +1,12 @@
 package com.okp4.processor.cosmos
 
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
-import org.apache.kafka.streams.kstream.*
+import org.apache.kafka.streams.kstream.Branched
+import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.Named
+import org.apache.kafka.streams.kstream.Produced
 import org.slf4j.LoggerFactory
 import tendermint.types.BlockOuterClass
 import java.util.*
@@ -24,58 +26,48 @@ fun topology(props: Properties): Topology {
         "Option 'topic.error' was not specified."
     }
 
-    return StreamsBuilder()
-        .apply {
-            stream(topicIn, Consumed.with(Serdes.String(), Serdes.ByteArray()).withName("input"))
-                .map(
-                    { k, v ->
-                        try {
-                            KeyValue(k, Pair(BlockOuterClass.Block.parseFrom(v).toByteArray(), null))
-                        } catch (e: Exception) {
-                            logger.error("Deserialization failed for block with key $k: ${e.message}")
-                            KeyValue(k, Pair(v, e.message))
-                        }
-                    }, Named.`as`("block-deserialization")
-                    )
-                    .split()
-                    .branch(
-                        { _, v -> !v.second.isNullOrEmpty() },
-                        Branched.withConsumer { ks ->
-                            ks.mapValues { pair ->
-                                pair.first
+    return StreamsBuilder().apply {
+        stream(topicIn, Consumed.with(Serdes.String(), Serdes.ByteArray()).withName("input")).mapValues(
+            { v ->
+                Pair(v, kotlin.runCatching { BlockOuterClass.Block.parseFrom(v) })
+            }, Named.`as`("block-deserialization")
+            ).split().branch(
+                { _, v -> v.second.isFailure },
+                Branched.withConsumer { ks ->
+                    ks.peek(
+                        { k, v ->
+                            v.second.onFailure {
+                                logger.warn("Deserialization failed for block with key <$k> (block will be sent to topic $topicError): ${it.message}", it)
                             }
-                                .to(
-                                    topicError, Produced.with(Serdes.String(), Serdes.ByteArray()).withName("error")
-                                )
-                        }
+                        },
+                        Named.`as`("log-deserialization-failure")
                     )
-                    .defaultBranch(
-                        Branched.withConsumer { ks ->
-                            ks.mapValues(
-                                { pair ->
-                                    BlockOuterClass.Block.parseFrom(pair.first)
-                                },
-                                Named.`as`("extract-block")
-                            )
-                                .peek(
-                                    { _, block -> logger.debug("→ block ${block.header.height} (${block.data.txsCount} txs)") },
-                                    Named.`as`("log")
-                                )
-                                .flatMapValues(
-                                    { block ->
-                                        block.data.txsList
-                                    }, Named.`as`("extract-transactions")
+                        .mapValues({ pair -> pair.first }, Named.`as`("extract-original-bytearray")).to(
+                            topicError, Produced.with(Serdes.String(), Serdes.ByteArray()).withName("error")
+                        )
+                    }
+                ).defaultBranch(
+                    Branched.withConsumer { ks ->
+                        ks.mapValues(
+                            { v ->
+                                v.second.getOrThrow()
+                            }, Named.`as`("extract-block")
+                            ).peek(
+                                { _, block -> logger.debug("→ block ${block.header.height} (${block.data.txsCount} txs)") },
+                                Named.`as`("log-block-extraction")
+                            ).flatMapValues(
+                                { block ->
+                                    block.data.txsList
+                                }, Named.`as`("extract-transactions")
+                                ).mapValues(
+                                    { tx ->
+                                        tx.toByteArray()
+                                    }, Named.`as`("convert-transactions-to-bytearray")
+                                    ).to(
+                                        topicOut, Produced.with(Serdes.String(), Serdes.ByteArray()).withName("output")
                                     )
-                                    .mapValues(
-                                        { tx ->
-                                            tx.toByteArray()
-                                        }, Named.`as`("convert-transactions-to-bytearray")
-                                        )
-                                        .to(
-                                            topicOut, Produced.with(Serdes.String(), Serdes.ByteArray()).withName("output")
-                                        )
                                 }
                             )
-                    }.build()
-            }
-            
+                        }.build()
+                    }
+                    
